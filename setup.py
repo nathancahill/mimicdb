f8238938bb17064725eebe80da88634c29b5dcb4
Nathan Cahill <nathan@churchillnavigation.com>
2015-04-20 15:24:17 -0600

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'MimicDB',
-    version = '1.0.3',
+    version = '1.0.4',
     packages = find_packages(),
     install_requires = ['boto>=2.32.1',],
     license = 'BSD',
